a2030adaae62bfefdd2c7c2546be2e63f32bb902
Ibrahim Fathi <himafathy796@gmail.com>
2023-06-08 20:29:45 +0300

    auth function done

--- auth/verify_code.php
+++ auth/verify_code.php
@@ -1,13 +1,13 @@
 <?php
 include "../connect.php";
-$phone = filterRequest("phone");
+$email = filterRequest("email");
 $userVerifyCode = filterRequest("verifycode");
-$stmt = $con -> prepare("SELECT * FROM users WHERE users_phone = '$phone' AND users_verify_code = '$userVerifyCode'");
+$stmt = $con -> prepare("SELECT * FROM users WHERE users_email = '$email' AND users_verify_code = '$userVerifyCode'");
 $stmt -> execute();
 $count = $stmt ->rowCount();
 if($count > 0){
     $data = array("users_approve" => "1");
-    updateData("users", $data, "users_phone = '$phone'");
+    updateData("users", $data, "users_email = '$email'");
 
 }else{
     printFailure("Verify Code is not Valid.");
--- forget_password/check_phone_number.php
+++ forget_password/check_phone_number.php
@@ -1,16 +1,16 @@
 <?php
 include "../connect.php";
-$email = filterRequest("email");
+$phone = filterRequest("phone");
 
 $verfiycode     = rand(10000, 99999);
 
-$stmt = $con->prepare("SELECT * FROM users WHERE users_email = ? ");
-$stmt->execute(array($email));
+$stmt = $con->prepare("SELECT * FROM users WHERE users_phone = ? ");
+$stmt->execute(array($phone));
 $count = $stmt->rowCount();
 printResult($count);
 
 if ($count > 0) {
     $data = array("users_verify_code" => $verfiycode);
-    updateData("users", $data, "users_email = '$email'", false);
+    updateData("users", $data, "users_phone = '$phone'", false);
     // sendEmail($email, "Verfiy Code Ecommerce", "Verfiy Code $verfiycode");
 }
