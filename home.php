7ccd806b24d50cc214db24f1b0b009bf1e7f18b3
Ibrahim Fathi <himafathy796@gmail.com>
2023-06-09 06:57:30 +0300

    home categories

--- home.php
+++ home.php
@@ -1,6 +1,7 @@
 <?php
 include "connect.php";
 $allData = array();
+$allData['status'] = "success";
 $categories = getAllData("categories",null,null,false);
 $allData['categories'] = $categories; 
 echo json_encode($allData);
