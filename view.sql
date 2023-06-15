bc56dd8ef80cce20f793b998227fb1d3d05f6034
Ibrahim Fathi <himafathy796@gmail.com>
2023-06-15 04:07:52 +0300

    favorite view

--- view.sql
+++ view.sql
@@ -1,3 +1,8 @@
-CREATE VIEW itemview AS
+CREATE OR REPLACE VIEW itemsview1 AS
 SELECT items.* , categories.* FROM items 
 INNER JOIN  categories on  items.items_categories = categories.categories_id
+
+CREATE OR REPLACE VIEW myfavorite AS 
+SELECT favorite.* , items.* , users.users_id FROM favorite
+INNER JOIN users ON users.users_id = favorite.favorite_users_id
+INNER JOIN items ON items.items_id = favorite.favorite_items_id
