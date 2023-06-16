97444bcbaf2acc9c7bbdd2077e2a6ac638240e82
Ibrahim Fathi <himafathy796@gmail.com>
2023-06-17 00:58:06 +0300

    cart view

--- view.sql
+++ view.sql
@@ -6,3 +6,8 @@ CREATE OR REPLACE VIEW myfavorite AS
 SELECT favorite.* , items.* , users.users_id FROM favorite
 INNER JOIN users ON users.users_id = favorite.favorite_users_id
 INNER JOIN items ON items.items_id = favorite.favorite_items_id
+
+CREATE OR REPLACE VIEW cartview AS
+SELECT SUM(items.items_price) AS itemsprice, COUNT(cart_items_id) AS itemscount, cart.* , items.* FROM cart
+INNER JOIN items ON items.items_id = cart.cart_items_id
+GROUP BY cart.cart_items_id, cart.cart_user_id
