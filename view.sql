4b60acf8dfbe89a0ec10a01c58d04c7c2b0512ae
Ibrahim Fathi <himafathy796@gmail.com>
2023-06-18 02:05:25 +0300

    discount

--- view.sql
+++ view.sql
@@ -8,6 +8,6 @@ INNER JOIN users ON users.users_id = favorite.favorite_users_id
 INNER JOIN items ON items.items_id = favorite.favorite_items_id
 
 CREATE OR REPLACE VIEW cartview AS
-SELECT SUM(items.items_price) AS itemsprice, COUNT(cart_items_id) AS itemscount, cart.* , items.* FROM cart
+SELECT SUM(items.items_price - (items.items_price * (items.items_discount/100))) AS itemsprice, COUNT(cart_items_id) AS itemscount, cart.* , items.* FROM cart
 INNER JOIN items ON items.items_id = cart.cart_items_id
 GROUP BY cart.cart_items_id, cart.cart_user_id
