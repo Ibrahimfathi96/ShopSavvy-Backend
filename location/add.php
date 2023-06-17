e20f0fec23bd96528f31a390a43505f3e79fab01
Ibrahim Fathi <himafathy796@gmail.com>
2023-06-18 02:30:30 +0300

    location

--- location/add.php
+++ location/add.php
@@ -2,12 +2,14 @@
 include"../connect.php";
 $table  = "location";
 $userId = filterRequest("userId");
+$name   = filterRequest("name");
 $city   = filterRequest("city");
 $street = filterRequest("street");
 $lat    = filterRequest("lat");
 $long   = filterRequest("long");
 $data   = array(
     "location_user_id" => $userId,
+    "location_name"    => $name,
     "location_city"    => $city ,
     "location_street"  => $street,
     "location_lat"     => $lat,
--- location/edit.php
+++ location/edit.php
@@ -3,11 +3,13 @@ include"../connect.php";
 $table  = "location";
 $locationId = filterRequest("location_id");
 $userId     = filterRequest("userId");
+$name   = filterRequest("name");
 $city       = filterRequest("city");
 $street     = filterRequest("street");
 $lat        = filterRequest("lat");
 $long       = filterRequest("long");
 $data       = array(
+    "location_name"    => $name,
     "location_city"    => $city ,
     "location_street"  => $street,
     "location_lat"     => $lat,
