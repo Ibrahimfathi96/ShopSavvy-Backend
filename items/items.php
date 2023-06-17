7cf2fba001e15b2906c71cbc34ec0bb893d9a01f
Ibrahim Fathi <himafathy796@gmail.com>
2023-06-18 01:28:39 +0300

    items discount

--- items/items.php
+++ items/items.php
@@ -5,11 +5,11 @@ $userid =  filterRequest("userid");
 // getAllData("itemsview","categories_id = $categoryID ");
 
 $stmt = $con->prepare("
-SELECT itemsview1.* , 1 AS favorite FROM itemsview1
+SELECT itemsview1.* , 1 AS favorite, (items_price - (items_price * (items_discount/100)))as itemspricediscount FROM itemsview1
 INNER JOIN favorite ON favorite.favorite_items_id = itemsview1.items_id AND favorite.favorite_users_id = $userid
 WHERE categories_id = $categoryID
 UNION ALL
-SELECT * , 0 AS favorite FROM itemsview1
+SELECT * , 0 AS favorite, (items_price - (items_price * (items_discount/100)))as itemspricediscount  FROM itemsview1
 WHERE categories_id = $categoryID AND items_id NOT IN (SELECT itemsview1.items_id FROM itemsview1
 INNER JOIN favorite ON favorite.favorite_items_id = itemsview1.items_id AND favorite.favorite_users_id = $userid)
 ");
