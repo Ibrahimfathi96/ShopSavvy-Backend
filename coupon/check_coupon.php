4a135cae1064e266379b3b81d7abcc2dc9bd54e3
Ibrahim Fathi <himafathy796@gmail.com>
2023-06-19 02:03:25 +0300

    check coupon

--- coupon/check_coupon.php
+++ coupon/check_coupon.php
@@ -1,6 +1,6 @@
 <?php
 include"../connect.php";
-$couponName = filterRequest("counponName");
+$couponName = filterRequest("couponName");
 $now = date("Y-m-d H:i:s");
 getData("coupon","coupon_name = '$couponName' AND coupon_expired > '$now' AND coupon_count > 0 ");
 ?>
