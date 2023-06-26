36c498ad723111089e4454e8acfe44130908c8ab
Ibrahim Fathi <himafathy796@gmail.com>
2023-06-27 02:00:39 +0300

    admin approve orders

--- admin/orders/approve.php
+++ admin/orders/approve.php
@@ -1,8 +1,10 @@
 <?php
 include "../../connect.php";
 $orderId = filterRequest("orderId");
+$userId  = filterRequest("userId");
 
 $data = array(
     "orders_status" => 1,
 );
 updateData("orders", $data, "orders_id = $orderId AND orders_status = 0");
+sendGCM("Approval","your order has been approved","user$userId","none","none");
