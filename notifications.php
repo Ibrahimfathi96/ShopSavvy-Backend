df5b41973b26cf986cf2bd94f5f0de35ddd20909
Ibrahim Fathi <himafathy796@gmail.com>
2023-06-30 04:13:18 +0300

    view sorting

--- notifications.php
+++ notifications.php
@@ -1,5 +1,5 @@
 <?php
 include "connect.php";
 $userId = filterRequest("userId");
-getAllData("notifications","notifications_user_id = $userId");
+getAllData("notifications","notifications_user_id = $userId ORDER BY `notifications`.`notifications_datetime` DESC");
 ?>
--- orders/pending.php
+++ orders/pending.php
@@ -1,5 +1,5 @@
 <?php
 include "../connect.php";
 $userId = filterRequest("userId");
-getAllData("ordersview", "orders_user_id = '$userId'");
+getAllData("ordersview", "orders_user_id = '$userId' ORDER BY `ordersview`.`orders_datetime` DESC");
 ?>
