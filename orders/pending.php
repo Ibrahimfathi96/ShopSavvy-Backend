0a29d1bb9b90ed7184583675dcfa867733dddd0b
Ibrahim Fathi <himafathy796@gmail.com>
2023-07-03 03:02:36 +0300

    archive

--- orders/pending.php
+++ orders/pending.php
@@ -1,5 +1,5 @@
 <?php
 include "../connect.php";
 $userId = filterRequest("userId");
-getAllData("ordersview", "orders_user_id = '$userId' ORDER BY `ordersview`.`orders_datetime` DESC");
+getAllData("ordersview", "orders_user_id = '$userId' AND orders_status != 4 ORDER BY `ordersview`.`orders_datetime` DESC");
 ?>
