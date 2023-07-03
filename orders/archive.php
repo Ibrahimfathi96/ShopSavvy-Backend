106e8dd722ad5cd0c1c741a4228e7984ffcef1ed
Ibrahim Fathi <himafathy796@gmail.com>
2023-07-03 18:03:35 +0300

    archive

--- orders/archive.php
+++ orders/archive.php
@@ -3,5 +3,3 @@ include "../connect.php";
 $userId = filterRequest("userId");
 getAllData("ordersview", "orders_user_id = '$userId' AND orders_status = 4 ORDER BY `ordersview`.`orders_datetime` DESC");
 ?>
-
-// 0 -> wait, 1-> preparing, 2-> with delivery man, 3-> on the way, 4-> archive
