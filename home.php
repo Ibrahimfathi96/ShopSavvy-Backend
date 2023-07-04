596796104efe18e6032e3d55216a9512bce14e19
Ibrahim Fathi <himafathy796@gmail.com>
2023-07-04 03:11:19 +0300

    top selling items

--- home.php
+++ home.php
@@ -4,7 +4,8 @@ $allData = array();
 $allData['status'] = "success";
 $categories = getAllData("categories",null,null,false);
 $allData['categories'] = $categories; 
-$items = getAllData("topsellingitems","items_discount != 0",null,false);
+// $items = getAllData("itemsview1","items_discount != 0",null,false);//for discount items
+$items = getAllData("topsellingitems","1 = 1 ORDER BY itemsCount DESC",null,false);
 $allData['items'] = $items; 
 echo json_encode($allData);
 
