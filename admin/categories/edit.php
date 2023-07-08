3b30b069782f9ef04321162ddb4fb12a2f91bdb1
Ibrahim Fathi <himafathy796@gmail.com>
2023-07-08 05:14:04 +0300

    [FEATURE] Admin backend

--- admin/categories/edit.php
+++ admin/categories/edit.php
@@ -6,12 +6,14 @@ $categoryId = filterRequest("categoryId");
 $categoryName  = filterRequest("categoryName");
 $categoryNameInArabic = filterRequest("categoryArName");
 $imageName   = imageUpload("../../uploads/categories", "files");
+$oldImage   = filterRequest("oldImage");
 if ($imageName == "empty") {
     $data = array(
         "categories_name" => $categoryName,
         "categories_name_ar" => $categoryNameInArabic,
     );
 } else {
+    deleteFile("../../uploads/categories", $oldImage);
     $data = array(
         "categories_name" => $categoryName,
         "categories_name_ar" => $categoryNameInArabic,
